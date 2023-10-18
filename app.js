b9509fd658b00903fa6b5155c0bdd9bddd525ae1
kimwoojin123 <kimwoojin1903@gmail.com>
2023-10-18 18:05:09 +0900

    서버 생성프로토

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ const mainPage = require("./module/mainpage");
 
 http
   .createServer((req, res) => {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
+    fs.readFile("index.html", "utf8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
     });
-    req.on("end", () => {
-      res.writeHead(200);
-      res.end(mainPage);
-    });
+
+    if (req.method === "POST") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+      req.on("end", () => {
+        res.writeHead(200);
+        res.end(mainPage);
+      });
+    }
   })
   .listen(8000, () => {
     console.log("http://localhost:8000");
--- module/mainpage.js
+++ module/mainpage.js
@@ -6,12 +6,8 @@ const mainPage = `<!DOCTYPE html>
   <title>Document</title>
 </head>
 <body>
-  <form method="POST" action="">
-    ID <input type="text" name="id"><br>
-    PW <input type="text" name="pw"><br>
-    PW2 <input type="text" name="pw2"><br>
-    Email <input type="text" name="email"><br>
-    <input type="submit" name="submit">
+  <form method="POST" action="/login">
+    ddd
   </form>
 </body>
 </html>`;
