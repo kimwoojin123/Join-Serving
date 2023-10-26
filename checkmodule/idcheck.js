3c340ca36e58271611011d85be66190d44116f46
kimwoojin123 <kimwoojin1903@gmail.com>
2023-10-26 10:05:26 +0900

    idcheck 모듈 수정

--- checkmodule/idcheck.js
+++ checkmodule/idcheck.js
@@ -1,15 +1,23 @@
 function idCheck(obj) {
-  let idStr = obj.split("");
-  let upIdStr = obj.toUpperCase();
-  let upId = upIdStr.split("");
-  let lowIdStr = obj.toLowerCase();
-  let lowId = lowIdStr.split("");
-  let arr = [];
-  let idUpper = idStr.filter((elem) => upId.includes(elem));
-  let idLower = idStr.filter((elem) => lowId.includes(elem));
-  if (idUpper.toString() !== arr.toString() && idLower.toString() !== arr.toString()) {
-    return true;
+  let hasLowerCase = false; // 소문자 포함 여부를 나타내는 변수
+  let hasUpperCase = false; // 대문자 포함 여부를 나타내는 변수
+
+  for (let i = 0; i < obj.length; i++) {
+    const char = obj[i];
+    if (char >= "a" && char <= "z") {
+      hasLowerCase = true;
+    } else if (char >= "A" && char <= "Z") {
+      hasUpperCase = true;
+    }
+
+    // 둘 다 발견되면 바로 true 반환
+    if (hasLowerCase && hasUpperCase) {
+      return true;
+    }
   }
+
+  // 루프가 끝났을 때 여전히 true가 아니라면 false 반환
+  return false;
 }
 
 module.exports = idCheck;
